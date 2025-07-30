9a1221de45d2a1b1301f0fc936e926caa0830f33
Saran P <saranp0864@gmail.com>
2025-07-30 12:05:49 +0530

    sentinel.py changed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routes.ndvi_timeseries import ndvi_router
+from routes.ndvi_timeseries import router
 
 app = FastAPI()
 
-app.include_router(ndvi_router)
+app.include_router(router)
